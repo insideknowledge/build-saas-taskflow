a75f5353c13055505a6d7446cbf0e6bf1e97fc4c
insideknowledge <insideknowledgesecret@gmail.com>
2025-05-13 17:51:29 +0530

    Updated sidebar.tsx

--- components/layout/sidebar.tsx
+++ components/layout/sidebar.tsx
@@ -102,8 +102,8 @@ export const Sidebar: FC<SidebarProps> = ({ className }) => {
 
   useEffect(() => {
     // Initialize audio elements
-    setHoverSound(new Audio('https://pixabay.com/sound-effects/mouse-click-290204/'));
-    setClickSound(new Audio('https://pixabay.com/sound-effects/mouse-click-290204/'));
+    setHoverSound(new Audio('https://www.soundjay.com/button/sounds/button-hover-1.mp3'));
+    setClickSound(new Audio('https://www.soundjay.com/buttons/sounds/button-16a.mp3'));
 
     // Cleanup function
     return () => {
